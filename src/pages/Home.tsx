30dcd0af4100bb56ad0cf9f3f7605574b0568d47
Colin Savage <colin.savage@purple.telstra.com>
2022-02-12 06:52:18 +1000

    Files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonDatetime, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -17,6 +17,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <IonDatetime></IonDatetime>
       </IonContent>
     </IonPage>
   );
