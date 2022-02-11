419b026825b9df6b509d372caa427ed250d740f4
Colin Savage <colin.savage@purple.telstra.com>
2022-02-11 11:23:59 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DateTest',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
